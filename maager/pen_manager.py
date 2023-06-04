ff19a5af0c6b80359ee7255944e0e94ef725ab1d
Melyandr <119205312+Melyandr@users.noreply.github.com>
2023-06-04 09:09:00 +0300

    added 10 laba

--- maager/pen_manager.py
+++ maager/pen_manager.py
@@ -4,8 +4,7 @@ from models.felttip_pen import FelttipPen
 from models.marker_pen import MarkerPen
 from models.officer_pen import OfficerPen
 from models.school_pen import SchoolPen
-from pathlib import Path
-from wrapt import decorator
+
 
 
 def decorator_file_write(func):
@@ -22,7 +21,7 @@ def decorator_file_write(func):
                 file.write(str(value))
                 file.write(", ")
             file.write("\n\n")
-
+        file.close()
         return result
 
     return wrapper
@@ -88,8 +87,7 @@ class PenManager:
     def enumerate_object(self, pen_list):  # level 1
         enum_objects = enumerate(pen_list)
         print("method enumerate_object works ")
-        for index, i in enum_objects:
-            print(index, i.__class__.__name__)
+        print([f'index:{index}, {i.__class__.__name__}'for index, i in enum_objects])
 
 
     def zip_method_realization(self, pen_list):  # level 1
@@ -97,18 +95,10 @@ class PenManager:
         price_list = self.output_price(pen_list)
         zip_objects = zip(pen_list, price_list)
         print(list(zip_objects))
-        for price, i in zip_objects:
-            print(f"{pen} {price} {price.__class__.__name__}")
+        for price in zip_objects:
+            print(f" {price.__class__.__name__} {price}")
 
-    # def decorator_file_write(func):
-    #     #     def inner(self, pen_object):
-    #     #         file = open("C:\\python\\laba7\\9laba.txt", "w")
-    #     #         result = func(pen_object)
-    #     #         file.write(str(result))
-    #     #         file.close()
-    #     #         return result
-    #     #
-    #     #     return inner
+
     @decorator_file_write
     @decorator_exception
     def is_digit(self, pen_object):  # level 1
@@ -117,6 +107,7 @@ class PenManager:
         print("function any()")
         are_there_digits = [char.isdigit() for char in pen_with_attribute]
         print(any(are_there_digits))
+        print("function all()")
         are_all_letters = [char.isalpha() for char in pen_with_attribute]
         print(all(are_all_letters))
 
@@ -133,14 +124,9 @@ if __name__ == "__main__":
     felttip_pen1 = FelttipPen("Kite", "orange", "cotton", 5, "for school", 10)
     felttip_pen2 = FelttipPen("Kite", "grey", "cotton", 3, "for building", 16)
     officer_pen1 = OfficerPen("nike", "white", "tree", 7, 1, 2, 3)
-    marker_pen1 = MarkerPen("puma", "black", "cloth", 4, "for school", 10)
-    officer_pen2 = OfficerPen("champion", "white", "tree", 5, 1, 2, 3)
+    marker_pen1 = MarkerPen("puma", "black", "cloth", -8, "for school", 10)
+    officer_pen2 = OfficerPen("champion", "white", "tree", 3, 1, 2, 3)
     marker_pen2 = MarkerPen("puma", "black", "cloth", 4, "for school", 10)
-
-    # print(felttip_pen1.calculate_price())
-    # print(school_pen1.calculate_price())
-    # print(officer_pen1.calculate_price())
-    # print(marker_pen1.calculate_price())
 
     pen_manager.add_pen(school_pen1)
     pen_manager.add_pen(felttip_pen1)
@@ -151,34 +137,39 @@ if __name__ == "__main__":
     pen_manager.add_pen(officer_pen2)
     pen_manager.add_pen(marker_pen2)
 
-    print(len(pen_manager))  # first level
-    print("\n")
-    print(pen_manager[3])  # first level
+    marker_pen1.is_exsist_pen()
 
-    print("\n\n")
-    for pen in pen_manager:  # first level
-        print(pen)
+    print(officer_pen2.calculate_price())
+    print(marker_pen1.calculate_price())
 
-    print("\n\n")
-    pen_manager.output_price(pen_manager.pen_list)
+    # print(len(pen_manager))  # first level
+    # print("\n")
+    # print(pen_manager[3])  # first level
+    #
+    # print("\n\n")
+    # for pen in pen_manager:  # first level
+    #     print(pen)
+    #
+    # print("\n\n")
+    # pen_manager.output_price(pen_manager.pen_list)
+    #
+    # print("\n\n")
+    # pen_manager.enumerate_object(pen_manager.pen_list)
+    #
+    # print("\n\n")
+    # pen_manager.zip_method_realization(pen_manager.pen_list)
+    #
+    # print("\n\n get_attributes_by_type works")
+    # attribute = school_pen1.get_attributes_by_type(str)
+    # print(attribute)
+    #
+    # print("\n\n")
+    #
+    # pen_manager.is_digit(pen_object=school_pen1)  # level 3
+    # pen_manager.is_digit(school_pen2)
+    # pen_manager.is_digit(felttip_pen1)
+    # # pen_manager.is_digit(felttip_pen2)
+    # # if i add this object i will catch Exception(too many arguments)
+    # print("vebebebe")
 
-    print("\n\n")
-    pen_manager.enumerate_object(pen_manager.pen_list)
 
-    print("\n\n")
-    pen_manager.zip_method_realization(pen_manager.pen_list)
-
-    print("\n\n get_attributes_by_type works")
-    attribute = school_pen1.get_attributes_by_type(str)
-    print(attribute)
-
-    print("\n\n")
-
-    pen_manager.is_digit(pen_object=school_pen1)  # level 3
-    pen_manager.is_digit(school_pen2)
-    pen_manager.is_digit(felttip_pen1)
-    # pen_manager.is_digit(felttip_pen2)
-    # if i add this object i will catch Exception(too many arguments)
-    print("vebebebe")
-
-    # pen_manager.get_brander(school_pen1)
--- models/pen.py
+++ models/pen.py
@@ -1,6 +1,46 @@
 from abc import ABC, abstractmethod
+from functools import wraps
+import logging
+from hashlib import new
 
+from Exception.invalid_value_exception import invalidValueException
+from Tools.scripts.var_access_benchmark import C
+from isort import output
 from wrapt import decorator
+from Exception import invalid_value_exception
+
+exc = invalidValueException()
+
+
+def logged(exception, mode):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except exception as e:
+                if mode == "console":
+                    logging.basicConfig(level=logging.ERROR)
+                    logging.error(e)
+                elif mode == "file":
+                    logging.basicConfig(filename='C:\output\wer.txt', filemode='w', level=logging.ERROR)
+                    logging.error(e)
+                else:
+                    raise exception("Invalid mode. Supported modes are 'console' and 'file'")
+                raise
+
+        return wrapper
+
+    return decorator
+
+
+# def logged(exc, file='C:\output\wer.txt'):
+#     def wrapper(func):
+#         if invalid_value_exception:
+#             logging.basicConfig(filename='C:\output\wer.txt', filemode='w',
+#                                 format='%(size)s - %(levelname)s - %(message)s')
+#             logging.error('This will get logged to a file')
+#             raise exc("You have written size op PEN what is less than 0 or equal 0")
+#         return wrapper
 
 
 class Pen(ABC):
@@ -26,7 +66,15 @@ class Pen(ABC):
         """
         it will calculate total price
         """
+        # if self.size <= 0:
+        #     raise ValueError("You have written size op PEN what is less than 0 or equal 0")
+        # else:
         pass
+
+    @logged(invalidValueException, mode="file")
+    def is_exsist_pen(self):
+        if self.size <= 0:
+            raise invalidValueException("size of PEN is less than 0")
 
     def get_attributes_by_type(self, data_type):
         return {key: value for key, value in self.__dict__.items() if isinstance(value, data_type)}
@@ -34,4 +82,3 @@ class Pen(ABC):
     @abstractmethod
     def people_who_use(self):
         pass
-
